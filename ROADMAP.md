a40a071fda58687bf497b87beb37febbe073afae
Linbudu <48507806+linbudu599@users.noreply.github.com>
2022-03-29 16:58:14 +0800

    Initial commit

---
+++ ROADMAP.md
@@ -0,0 +1,19 @@
+# Roadmap
+
+## Incoming Starters
+
+## Advanced(Stage 3)
+
+- StoryBook
+- Midway Hooks
+- Midway Serverless
+- Nest
+- Nest + GraphQL
+- MidwayJS + GraphQL
+- GenQL
+- RPC
+- Node worker_threads
+- serverless
+- Mobx -> React
+- MobxStateTree + GraphQL
+- Workflow: GitHub Actions
---
+++ scripts/init-workspace.ts
@@ -0,0 +1,52 @@
+import { CAC } from 'cac';
+import consola from 'consola';
+import fs from 'fs-extra';
+import ora from 'ora';
+import { CLIUtils, Constants } from './utils';
+
+export default function useInitWorkspaceAfterInstall(cli: CAC) {
+  cli
+    .command('init', 'pick starter to init minimal workspace', {
+      allowUnknownOptions: true,
+    })
+    .action(async () => {
+      const existPackages = CLIUtils.existPackages;
+
+      if (!existPackages.length) {
+        consola.warn('No package found in current workspace, exit.');
+        process.exit(0);
+      }
+
+      const chosedStarters = await CLIUtils.createPackageMultiSelector(
+        'chosedStarters',
+        'Pick starters to initialize workspace'
+      );
+
+      const excluded = chosedStarters.includes(Constants.noneIdentifier)
+        ? existPackages
+        : existPackages.filter((p) => !chosedStarters.includes(p));
+
+      for (const project of excluded) {
+        const projectSrcPath = CLIUtils.resolvePackageDir(project);
+
+        fs.removeSync(projectSrcPath);
+      }
+
+      consola.success(
+        `Workspace initialized, executing deps install for selected packages...`
+      );
+
+      const spinner = ora('Installing dependencies...').start();
+
+      CLIUtils.useChildProcess(
+        'pnpm install --registry=https://registry.npmmirror.com --prefer-offline',
+        {
+          stdio: 'ignore',
+        }
+      ).then(() => {
+        spinner.succeed('Dependencies installed!');
+        console.log('');
+        consola.success('Enjoy!');
+      });
+    });
+}
